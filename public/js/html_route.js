a89aed92a5637e4f5941d0f963bc6cba2e706150
Shauna Dunn <shaunadunn1@gmail.com>
2021-02-18 22:13:21 -0500

    heroku troubleshooting

--- public/js/html_route.js
+++ public/js/html_route.js
@@ -9,4 +9,7 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../notes.html
   
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-}
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "../public/index.html"));
+
+})};
